abd2d3c2cc0f1277b66d7cb8d0db12f05de861c5
Jean Castilho <jean@smart4sistemas.com>
2023-10-04 09:42:58 -0300

    Project created

---
+++ src/test/java/com/castilhocorp/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.castilhocorp.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
