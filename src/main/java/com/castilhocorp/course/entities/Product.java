ac1dae0200b848088e1dc71879c58824d114a814
Jean Castilho <jean@smart4sistemas.com>
2023-10-11 15:55:59 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/castilhocorp/course/entities/Product.java
+++ src/main/java/com/castilhocorp/course/entities/Product.java
@@ -5,6 +5,8 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -13,6 +15,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 
@@ -33,6 +36,9 @@ public class Product implements Serializable{
 	@ManyToMany
 	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
+	
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
 														 
 	public Product() {
 	}
@@ -71,12 +77,12 @@ public class Product implements Serializable{
 		this.description = description;
 	}
 
-	public Double getPriceDouble() {
+	public Double getPrice() {
 		return price;
 	}
 
-	public void setPriceDouble(Double priceDouble) {
-		this.price = priceDouble;
+	public void setPrice(Double price) {
+		this.price = price;
 	}
 
 	public String getImgUrl() {
@@ -89,6 +95,15 @@ public class Product implements Serializable{
 
 	public Set<Category> getCategories() {
 		return categories;
+	}
+	
+	@JsonIgnore
+	public Set<Order> getOrders(){
+		Set<Order> set = new HashSet<>();
+		for (OrderItem x: items) {
+			set.add(x.getOrder());
+		}
+		return set;
 	}
 
 	@Override
